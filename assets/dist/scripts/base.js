0d92c1f1ec0e01333aa03c8604581f4f4c744ca9
addzyorbital <adam@orbital.co.uk>
2019-08-19 14:57:02 +0100

    Added gulp workflow for js

--- assets/dist/scripts/base.js
+++ assets/dist/scripts/base.js
@@ -1,1 +1,2 @@
-console.log('boom');
+
+//# sourceMappingURL=base.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var autoprefixer = require('autoprefixer'),
     calc = require('postcss-calc'),
     cssnano = require('cssnano'),
+    concat = require('gulp-concat'),
     gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     postcssPresetEnv = require('postcss-preset-env'),
     plumber = require('gulp-plumber'),
+    jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
+    stylish = require('jshint-stylish'),
     stylelint = require('stylelint'),
+    uglify = require('gulp-uglify'),
     gutil = require('gulp-util');
 
 const postcssPlugins = [
@@ -43,13 +47,29 @@ gulp.task('css', function() {
         .pipe(gulp.dest('./assets/dist/styles/'));
 });
 
+// JSHint, concat, and minify JavaScript
+gulp.task('scripts', function() {
+    return gulp
+        .src('./assets/src/scripts/**/*.js')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(concat('base.js'))
+        .pipe(gulp.dest('./assets/dists/scripts'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./assets/dist/scripts'));
+});
+
 // Watch files for changes (without Browser-Sync)
 gulp.task('watch', function() {
     // Watch .scss files
     gulp.watch('./assets/src/styles/**/*.scss', gulp.series('css'));
+    gulp.watch('./assets/src/scripts/**/*.js', gulp.series('scripts'));
 });
 
 // Run fed-styles, site-js and foundation-js
 gulp.task('default', function() {
-    gulp.start('css');
+    gulp.start('css', 'scripts');
 });
