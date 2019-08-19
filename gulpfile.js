6734fe52c3e171d5801c2598d1e7968c07619ea0
addzyorbital <adam@orbital.co.uk>
2019-08-19 15:42:04 +0100

    Added img minification process to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var autoprefixer = require('autoprefixer'),
     postcssPresetEnv = require('postcss-preset-env'),
     plumber = require('gulp-plumber'),
     jshint = require('gulp-jshint'),
+    imagemin = require('gulp-imagemin'),
+    newer = require('gulp-newer'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     stylish = require('jshint-stylish'),
@@ -62,14 +64,38 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('./assets/dist/scripts'));
 });
 
+// Optimize Images
+gulp.task('images', function() {
+    return gulp
+        .src('./assets/src/img/**/*')
+        .pipe(newer('./assets/dist/img'))
+        .pipe(
+            imagemin([
+                imagemin.gifsicle({ interlaced: true }),
+                imagemin.jpegtran({ progressive: true }),
+                imagemin.optipng({ optimizationLevel: 3 }),
+                imagemin.svgo({
+                    plugins: [
+                        {
+                            removeViewBox: false,
+                            collapseGroups: true
+                        }
+                    ]
+                })
+            ])
+        )
+        .pipe(gulp.dest('./assets/dist/img'));
+});
+
 // Watch files for changes (without Browser-Sync)
 gulp.task('watch', function() {
     // Watch .scss files
     gulp.watch('./assets/src/styles/**/*.scss', gulp.series('css'));
     gulp.watch('./assets/src/scripts/**/*.js', gulp.series('scripts'));
+    gulp.watch('./assets/src/img/**/*', gulp.series('images'));
 });
 
 // Run fed-styles, site-js and foundation-js
 gulp.task('default', function() {
-    gulp.start('css', 'scripts');
+    gulp.start('css', 'scripts', 'images');
 });
